cfc610989fed18cd2600d593ea4b226832aedc2d
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2025-04-26 12:08:49 +0500

    1

--- Freelance/settings.py
+++ Freelance/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qyfu#dk*x$xbev9_u2%ze)nn^+x0)fi-3)m^cklrwc+p-%u4n0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
