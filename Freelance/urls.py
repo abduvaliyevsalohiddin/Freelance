30ad78fcd62782be5f6130ff194ef2fd30443316
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2025-04-27 17:47:06 +0500

    chagnge urls/schema

--- Freelance/urls.py
+++ Freelance/urls.py
@@ -1,14 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from userApp.views import ProfileRegistrationView
+from rest_framework import permissions
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView, )
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,20 +16,19 @@ schema_view = get_schema_view(
         description="Test FreelancePlatform API",
         contact=openapi.Contact("Abduvaliyev Salohiddin. Email: abduvaliyevsalohiddin568@gmail.com")
     ),
-    public=True
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('mainapp/', include('mainApp.urls')),
-    path('userapp/', include('userApp.urls')),
+                  path('admin/', admin.site.urls),
+                  path('mainapp/', include('mainApp.urls')),
+                  path('userapp/', include('userApp.urls')),
 
+                  path('', schema_view.with_ui('swagger', cache_timeout=0)),
 
-    path('', schema_view.with_ui('swagger', cache_timeout=0)),
+                  path('register/', ProfileRegistrationView.as_view()),
+                  path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('register/', ProfileRegistrationView.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
